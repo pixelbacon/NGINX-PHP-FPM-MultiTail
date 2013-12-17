c16ae919d1c32efd0b7966ec7d8f26875b8590c2
Michael Minor <mike@doejo.com>
2013-12-17 16:01:26 -0600

    Initial commit

---
+++ www-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+>/var/log/nginx/error.log
+>/var/log/nginx/access.log
+>/var/log/php5-fpm.log
+sudo service nginx restart
+sudo service php5-fpm restart
+if which multi tail >/dev/null; then
+  sudo apt-get install multitail 
+else
+  echo multitail installed
+fi
+sudo multitail /var/log/php5-fpm.log /var/log/nginx/access.log /var/log/nginx/error.log
