87b09b316f507ff478952d6aab34cd106b2e0190
Michael Minor <mike@doejo.com>
2013-12-18 10:45:53 -0600

    Derp. Bug fix.

--- www-logs.sh
+++ www-logs.sh
@@ -4,9 +4,10 @@
 >/var/log/php5-fpm.log
 sudo service nginx restart
 sudo service php5-fpm restart
-if which multitail >/dev/null; then
-  sudo apt-get install multitail 
+if dpkg-query -l multitail; then
+  echo mutlitail install
 else
-  echo multitail installed
+  echo multitail not installed. Fixing that.
+  sudo apt-get  install multitail
 fi
 sudo multitail /var/log/php5-fpm.log /var/log/nginx/access.log /var/log/nginx/error.log
